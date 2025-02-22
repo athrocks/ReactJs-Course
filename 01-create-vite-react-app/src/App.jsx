3239d6f4e12ba1cc7b2c60da94825e47f2b74a21
athrocks <atharva0709pardeshi@gmail.com>
2025-02-22 14:57:11 +0530

    second Commit

--- 01-create-vite-react-app/src/App.jsx
+++ 01-create-vite-react-app/src/App.jsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1>Chai Aur React with vite | Atharva</h1>
-  )
+  return <h1>Chai Aur React with vite | Atharva</h1>;
 }
 
-export default App
+export default App;
