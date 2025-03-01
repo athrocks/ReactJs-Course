2acec93b8380efb944c15369c6f96b46f5ad0954
athrocks <atharva0709pardeshi@gmail.com>
2025-03-01 21:11:32 +0530

    configure redux toolkit

--- 14-Mega-Blog-project/src/App.jsx
+++ 14-Mega-Blog-project/src/App.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) dispatch(login({ userData }));
+        else dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (!loading) {
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>;
+  } else {
+    return null;
+  }
 }
 
 export default App;
---
+++ 14-Mega-Blog-project/src/store/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from '../store/authSlice';
+
+const store = configureStore({
+    reducer: {
+
+    }
+});
+
+export default store;
